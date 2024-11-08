26c1c5ea4373c0c285c19d2c21f8d319a1cc2fda
username-perverz-modon <cimalolnak@gmail.com>
2024-11-08 13:19:14 +0100

    upload this extra

--- src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
+++ src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
@@ -1,8 +1,14 @@
 package hu.bme.mit.spaceship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.validateMockitoUsage;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class TorpedoStoreTest {
 
@@ -16,5 +22,84 @@ class TorpedoStoreTest {
 
         // Assert
         assertEquals(true, result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-2, -1, 0, 1, 2})
+    void fireIllegalWithEmptySource(int numberOfTorpedos){
+        TorpedoStore store = new TorpedoStore(0);
+
+        assertTrue(store.isEmpty());
+        assertEquals(0, store.getTorpedoCount());
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            store.fire(numberOfTorpedos);
+        });
+        
+        assertTrue(store.isEmpty());
+        assertEquals(0, store.getTorpedoCount());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-2, -1, 0, 2, 5})
+    void fireIllegalWithASingleTorpedoStore(int numberOfTorpedos){
+        TorpedoStore store = new TorpedoStore(1);
+        
+        assertFalse(store.isEmpty());
+        assertEquals(1, store.getTorpedoCount());
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            store.fire(numberOfTorpedos);
+        });
+
+        assertFalse(store.isEmpty());
+        assertEquals(1, store.getTorpedoCount());
+    }
+
+    @Test
+    void fireValidWithASingleTorpedoStore(){
+        TorpedoStore store = new TorpedoStore(1);
+
+        assertFalse(store.isEmpty());
+        assertEquals(1, store.getTorpedoCount());
+
+        assertEquals(true, store.fire(1));
+
+        assertTrue(store.isEmpty());
+        assertEquals(0, store.getTorpedoCount());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-2, -1, 0, 4, 5})
+    void fireIllegalWithAMultipleTorpedoesStore(int numberOfTorpedos){
+        TorpedoStore store = new TorpedoStore(3);
+
+        assertFalse(store.isEmpty());
+        assertEquals(3, store.getTorpedoCount());
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            store.fire(numberOfTorpedos);
+        });
+        
+        assertFalse(store.isEmpty());
+        assertEquals(3, store.getTorpedoCount());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void fireValidWithAMultipleTorpedoesStore(int numberOfTorpedos){
+        TorpedoStore store = new TorpedoStore(3);
+
+        assertFalse(store.isEmpty());
+        assertEquals(3, store.getTorpedoCount());
+
+        assertEquals(true, store.fire(numberOfTorpedos));
+
+        if(numberOfTorpedos == 3){
+            assertTrue(store.isEmpty());
+        }else{
+            assertFalse(store.isEmpty());
+        }
+        assertEquals(3 - numberOfTorpedos, store.getTorpedoCount());
     }
 }
